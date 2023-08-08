3e5bb7c28a3a294e5d376b5690854172d526c81d
feijoes <pcaladomoura@gmail.com>
2023-08-07 21:43:16 -0300

    add requirements.txt

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import subprocess
 import eza_farming_bot
-from time import sleep
     
 if __name__ == "__main__":
-    print("\t\tProgram starting\n")
+    print("\tProgram starting\n")
     eza_farming_bot.start()
 
     
