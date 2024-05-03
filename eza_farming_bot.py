380fee38cec16a8c8d80eb285e02c451836d5d1b
kwr800 <lasiolaszewski.05@gmail.com>
2024-05-03 10:17:49 +0200

    Made the program try to complete the tasks multiple times instead of stopping

--- eza_farming_bot.py
+++ eza_farming_bot.py
@@ -1,4 +1,3 @@
-from time import sleep
 from adbutils import adb
 from adbutils._device import AdbDevice
 import cv2
@@ -7,11 +6,32 @@ import pytesseract
 from PIL import Image
 from typing import Callable
 import datetime
-def error(text)-> None:
-    print(text)
-    
-    quit()
-    
+from functools import wraps
+import time
+
+def log_error(message):
+    """Log an error message with a timestamp."""
+    with open("error_log.txt", "a") as log_file:
+        log_file.write(f"{datetime.datetime.now()}: {message}\n")
+
+def retry(retries=5, wait=2):
+    """Decorator to retry a method call with specified retries and wait time."""
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            nonlocal retries, wait
+            attempts = retries
+            while attempts > 0:
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    log_error(f"Error during {func.__name__}: {str(e)}")
+                    time.sleep(wait)
+                    attempts -= 1
+            return False  # Return False after all retries fail
+        return wrapper
+    return decorator
+
 def find_image_position(template_image: str,screenshot, threshold=0.8):
     # Load the target image and the template image
 
@@ -50,6 +70,7 @@ PRINT_TEXT = {
     "./Images/START.jpeg": "Click to confirm battle",
     "./Images/End.jpeg": "Battle ends",
     "./Images/OK.jpeg": "Click OK button",
+    "./Images/OK2.jpeg": "Click OK button",
     "./Images/CANCEL.jpeg": "CLick in Cancel button",
     "./Images/EZA.jpeg": "CLick to select EZA",
     "./Images/EXIT.jpeg": "Click to exit EZA",
@@ -61,26 +82,43 @@ import sys
 def delete_last_line():
     sys.stdout.write('\x1b[1A')
     sys.stdout.write('\x1b[2K')
+
     
 class EZA():
+
+    @retry(retries=3, wait=1)
+    def OK2(self, trys=30, raise_error: bool=True):
+        """Handle OK2 clicks with retries."""
+        image_path = "./Images/OK2.jpeg"
+        if not self._find_and_click(image_path, trys):
+            if raise_error:
+                self.device.screenshot().save(f"ERROR_{datetime.datetime.now().strftime('%H_%M_%S')}.jpeg")
+                log_error(f"Template {image_path} is not present in the target image.")
+            return False
+        return True
+
+    def handle_friend_request(self):
+        if not self.OK2(trys=2, raise_error=False):
+            print("Fallback to generic OK")
+            self.OK(trys=2, raise_error=False)
     
     def __init__(self, device: AdbDevice, debug: bool=False) -> None:
         self.device = device
         self.debug = debug
 
-    def _find_and_click(self, image_path: str, trys=30,wait:int=1,special=0):
-        """Return True if success else False """
+    @retry(retries=3, wait=1)
+    def _find_and_click(self, image_path: str, trys=30, wait=1, special=0):
+        """Attempts to find and click on an image, retrying on failure."""
         for _ in range(trys):
             find, x_pos, y_pos = self._find_image_position(image_path)
             if find:
-                print(f"{PRINT_TEXT[image_path]}",end="\r")
-                self.device.click(x_pos-special, y_pos)
+                print(f"{PRINT_TEXT[image_path]}", end="\r")
+                self.device.click(x_pos - special, y_pos)
                 return True
-            
-            for i in range(1,wait+1):
-                print("Waiting loading"+ "." * (i % 4))
+            for i in range(1, wait + 1):
+                print("Waiting loading" + "." * (i % 4))
                 delete_last_line()
-                sleep(1)
+                time.sleep(1)
         return False
     
     def _find(self, image_path: str, trys=30,wait:int=1):
@@ -89,7 +127,7 @@ class EZA():
             find, _, _ = self._find_image_position(image_path)
             if find:
                 return True
-            sleep(wait)
+            time.sleep(wait)
         return False
 
     def _find_dual_images(self, image_path_1: str, image_path_2: str, trys=30, wait=1):
@@ -119,73 +157,85 @@ class EZA():
                 else:
                     print("Waiting loading" + "." * (i % 4))
                 delete_last_line()
-                sleep(1)
+                time.sleep(1)
         return None
     def _find_image_position(self, image_path: str):
         screenshot = np.array(self.device.screenshot().convert('RGB'))
         template_image = cv2.imread(image_path)
         return find_image_position(template_image,screenshot)
-
-    def SelectLevel(self, isLREZA: bool ,trys=30, raise_error: bool=True):
+        
+    @retry(retries=3, wait=1)
+    def SelectLevel(self, isLREZA: bool , trys=30, raise_error: bool=True):
+        """Select level with retry logic."""
         image_path = "./Images/EZA.jpeg" if not isLREZA else "./Images/LREZA.jpeg"
         if not self._find_and_click(image_path, trys):
             if raise_error:
                 self.device.screenshot().save(f"ERROR_{datetime.datetime.now().strftime('%H_%M_%S')}.jpeg")
-                error(f"Template {image_path} is not present in the target image.")
+                log_error(f"Template {image_path} is not present in the target image.")
             return False
         return True
+
+    @retry(retries=3, wait=1)
     def ExitLevel(self, trys=30, raise_error: bool=True):
-        image_path = "./Images/EXIT.jpeg"
-        if not self._find_and_click(image_path, trys):
+        """Exit level with retries on fail."""
+        if not self._find_and_click("./Images/EXIT.jpeg", trys):
             if raise_error:
                 self.device.screenshot().save(f"ERROR_{datetime.datetime.now().strftime('%H_%M_%S')}.jpeg")
-                error(f"Template {image_path} is not present in the target image.")
+                log_error(f"Template {image_path} is not present in the target image.")
             return False
         return True
+
+    @retry(retries=3, wait=1)
     def Fight(self, trys=30, raise_error: bool=True):
+        """Initiate a fight, retrying on initial failures."""
         image_path = "./Images/FIGHT2.jpeg"
         if not self._find_and_click(image_path, trys):
             if raise_error:
                 self.device.screenshot().save(f"ERROR_{datetime.datetime.now().strftime('%H_%M_%S')}.jpeg")
-                error(f"Template {image_path} is not present in the target image.")
+                log_error(f"Template {image_path} is not present in the target image.")
             return False
         return True
+
+    @retry(retries=3, wait=1)
     def Start(self, trys=30, raise_error: bool=True):
+        """Start a level, with retries on failures."""
         image_path = "./Images/START.jpeg"
         if not self._find_and_click(image_path, trys):
-            
             if raise_error:
                 self.device.screenshot().save(f"ERROR_{datetime.datetime.now().strftime('%H_%M_%S')}.jpeg")
-                error(f"Template {image_path} is not present in the target image.")
+                log_error(f"Template {image_path} is not present in the target image.")
             return False
         return True
+
+    @retry(retries=3, wait=1)
     def OK(self, trys=30, raise_error: bool=True):
-        image_path = "./Images/OK.jpeg"
-        if not self._find_and_click(image_path, trys):
+        """Click OK with retries."""
+        if not self._find_and_click("./Images/OK.jpeg", trys):
             if raise_error:
                 self.device.screenshot().save(f"ERROR_{datetime.datetime.now().strftime('%H_%M_%S')}.jpeg")
-                error(f"Template {image_path} is not present in the target image.")
+                log_error(f"Template {image_path} is not present in the target image.")
             return False
         return True
+    @retry(retries=3, wait=1)
     def End(self, trys=45):
-        image_path_1 = "./Images/End.jpeg"
-        image_path_2 =  "./Images/FIGHT2.jpeg"
-        battle_end = self._find_dual_images(image_path_1, image_path_2, trys, wait=10)
+        """Handle end of battle with retry logic."""
+        battle_end = self._find_dual_images("./Images/End.jpeg", "./Images/FIGHT2.jpeg", trys, wait=10)
         if battle_end == None:
             self.device.screenshot().save(f"ERROR_{datetime.datetime.now().strftime('%H_%M_%S')}.jpeg")
-            error(f"Template {image_path_1} or {image_path_2} is not present in the target image.")
+            log_error("Neither 'End' nor 'Fight2' template is present in the target image.")
         elif battle_end[0] == 0:
             self.device.click(battle_end[1]-150, battle_end[2])
         elif battle_end[0] == 1:
             return False
         return True
     
+    @retry(retries=3, wait=1)
     def Cancel(self, trys=30, raise_error: bool=True):
-        image_path = "./Images/CANCEL.jpeg"
-        if not self._find_and_click(image_path, trys,wait=5):
+        """Attempt to cancel with retry logic."""
+        if not self._find_and_click("./Images/CANCEL.jpeg", trys, wait=5):
             if raise_error:
                 self.device.screenshot().save(f"ERROR_{datetime.datetime.now().strftime('%H_%M_%S')}.jpeg")
-                error(f"Template {image_path} is not present in the target image.")
+                log_error(f"Template {image_path} is not present in the target image.")
             return False
         return True
 
@@ -194,48 +244,41 @@ class EZA():
         print("Clicking at the center of the screen.",end="\r")
         self.device.click(x / 2,y / 2)
         
-    def get_level(self, zone: int = 1)-> int:
+    @retry(retries=3, wait=1)
+    def get_level(self, zone: int = 1) -> int:
+        """Get the current level using OCR with retries on fail."""
         screenshot = np.array(self.device.screenshot())
         pil_image = Image.fromarray(screenshot)
-        
-        # Crop the image to the specified region of interest
-        if zone == 1: x1, y1, x2, y2 = 890, 570, 1010, 630
+        if zone == 1:
+            x1, y1, x2, y2 = 890, 570, 1010, 630
         else:
-            _, x , y = self._find_image_position("./Images/NEXT.jpeg")
+            _, x, y = self._find_image_position("./Images/NEXT.jpeg")
             if not x:
                 x1, y1, x2, y2 = 890, 570, 1010, 630
             else:
-                x1, y1, x2, y2 = x-40, y+40 , x+80, y + 100
+                x1, y1, x2, y2 = x - 40, y + 40, x + 80, y + 100
         cropped_image = pil_image.crop((x1, y1, x2, y2))
-        
-        # Convert the cropped image to grayscale
         gray_cropped_image = cropped_image.convert('L')
-
-        # Perform thresholding on the grayscale image
-        thresh_image = cv2.threshold(src=np.array(gray_cropped_image), thresh=0, maxval=255, type=cv2.THRESH_OTSU + cv2.THRESH_BINARY_INV)[1]
-        
-        # Display the cropped image for testing
-        
-
-        # Perform OCR on the thresholded image
-        result: str = pytesseract.image_to_string(thresh_image, config="--psm 7 output digits")
-        if self.debug: 
-            print(result)
-            cropped_image.show()
-        if result[0]:
-            return int(result.split()[0])
-        cropped_image.save(f"ERROR_{datetime.datetime.now().minute}.jpeg")
-        quit("Unknow Eza level")
+        thresh_image = cv2.threshold(np.array(gray_cropped_image), 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY_INV)[1]
+        result = pytesseract.image_to_string(thresh_image, config="--psm 7 digits")
+        try:
+            level = int(result.split()[0])
+            return level
+        except (IndexError, ValueError):
+            log_error("Failed to read level from OCR result.")
+            return 1  # Default to level 1 if OCR fails
     
     def Swipe(self):
         x , y = self.device.window_size()
         self.device.swipe((x/2)+100,(y/2)+300,(x/2)-100,(y/2)+450,0.5)
     
+    @retry(retries=3, wait=1)
     def WaitUntil(self, image_path: str, function: Callable[[], None], wait:int, trys=10, raise_error: bool=True):
+        """Wait for an image and perform an action, with retry logic."""
         if not self._find(image_path, trys, wait):
             if raise_error:
                 self.device.screenshot().save(f"ERROR_{datetime.datetime.now().strftime('%H_%M_%S')}.jpeg")
-                error(f"Template {image_path} is not present in the target image.")
+                log_error(f"Template {image_path} is not present in the target image.")
             return False
         function()
         return True
@@ -249,48 +292,41 @@ class EZA():
 import os
 
 def start(debug:bool):
-    
     device: AdbDevice = adb.device()
     eza = EZA(device,debug=debug)
     n = 0
     while True:
-        sleep(0.5)
-        # if error in  get_level() change number to 1
-        
+        time.sleep(0.5)
         level: int = eza.get_level(2)
         maxlevel = 11 if eza.isLR() else 31
         if level < maxlevel:
-        
-            print("Start eza",end="\r")
+            print("Start eza", end="\r")
             eza.SelectLevel(isLREZA=maxlevel==11)
-            for _ in range(maxlevel-level):
+            for _ in range(maxlevel - level):
                 print(f"Current levels complete: {n}")
                 print("============================================")
-                sleep(0.5)
+                time.sleep(0.5)
                 eza.Fight()
-                sleep(1)
+                time.sleep(1)
                 eza.Start()
-                sleep(1)
-                
-                if not eza.End(35):
-                    print("Batlle lost , change eza")
-                    break 
-                sleep(1.5)
-               
-                sleep(1)
-                if not eza.Cancel(trys=2,raise_error=False):
-                    eza.OK()
-                    eza.OK(trys=2,raise_error=False)
-                sleep(1.5)
+                time.sleep(1)
+                if not eza.End(50):
+                    print("Battle lost, change eza")
+                    break
+                time.sleep(1.5)
+                eza.OK()
+                time.sleep(1)
+                if not eza.Cancel(trys=2, raise_error=False):
+                    eza.handle_friend_request()
+                time.sleep(1.5)
                 eza.click_center_screen()
-                n+=1
+                n += 1
                 os.system('cls' if os.name == 'nt' else 'clear')  
             eza.ExitLevel()
-            
+        
         print("Change EZA")
-        
-        eza.WaitUntil("./Images/EZA.jpeg",trys=30,function=eza.Swipe, wait=5) 
-        
+        eza.WaitUntil("./Images/EZA.jpeg", trys=30, function=eza.Swipe, wait=5) 
+
         
 
 def inf(no_lost:bool):
@@ -302,25 +338,23 @@ def inf(no_lost:bool):
     while True:
         print(f"Current levels complete: {n}\n============================================")
         n+=1
-        sleep(0.5)
+        time.sleep(0.5)
        
         eza.Fight()
-        sleep(1)
+        time.sleep(1)
         eza.Start()
-        sleep(1)
+        time.sleep(1)
         if not eza.End(50):
             print("Battle lost")
             if not no_lost:
                 break
             continue
-        sleep(1.5)
+        time.sleep(1.5)
         eza.OK()
-        sleep(1)
+        time.sleep(1)
         if not eza.Cancel(trys=2, raise_error=False):
-            print("not found cancel")
-            eza.OK()
-            eza.OK(trys=2,raise_error=False)
-        sleep(1)
+            eza.handle_friend_request()
+        time.sleep(1)
         eza.click_center_screen()
         os.system('cls' if os.name == 'nt' else 'clear')  
 
