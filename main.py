15daa650b96d7602e097bf2f6eb9736148b358b5
feijoes <pcaladomoura@gmail.com>
2024-09-22 14:32:29 -0300

    run format

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import eza_farming_bot
 import sys
+
 if __name__ == "__main__":
     print("###   Program starting   ###\n")
-    
+
     if "inf" in sys.argv:
-        no_lost =  "--nolost" in sys.argv
+        no_lost = "--nolost" in sys.argv
         eza_farming_bot.inf(no_lost=no_lost)
     else:
         debug_mode = "debug" in sys.argv
-        
-        eza_farming_bot.start(debug=debug_mode)
 
-    
-    
+        eza_farming_bot.start(debug=debug_mode)
